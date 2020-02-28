c4e735f25dbe6b11cb728ece826fc2d20082b755
Jaimin020 <48099245+Jaimin020@users.noreply.github.com>
2020-02-28 12:13:36 +0530

    examination

---
+++ UniversityExaminationMVC/UniversityExaminationMVC/Models/Branch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversityExaminationMVC.Models
+{
+    public class Branch
+    {
+        [Key]
+        public int Branch_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ UniversityExaminationMVC/UniversityExaminationMVC/Models/Exam.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversityExaminationMVC.Models
+{
+    public interface Exam_Operation
+    {
+        void Create_Exam();
+        void Schedule_Exam();
+        void Addpapers();
+    }
+
+    public class Opertaion_On_Exam : Exam_Operation
+    {
+        Exam e;
+        Opertaion_On_Exam(string et)
+        {
+            e=
+        }
+        public void Addpapers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create_Exam()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Schedule_Exam()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Exam_Factory
+    {
+        public void GetExam(string name)
+        {
+            if(name.Equals(""))
+            {
+
+            }
+        }
+    }
+
+    public class Exam
+    {
+        [Key]
+        public int Exam_Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string type { get; set; }
+        [Required]
+        public string sem { get; set; }
+        public Branch branch { get; set; }
+    }
+
+    public class Sessional_Exam:Exam
+    {
+        Sessional_Exam()
+        {
+            Name = "Sessional";
+            type = "Internal";
+        }
+    }
+
+    public class External_Exam : Exam
+    {
+        External_Exam()
+        {
+            Name = "Extnal";
+            type = "External";
+        }
+    }
+}
